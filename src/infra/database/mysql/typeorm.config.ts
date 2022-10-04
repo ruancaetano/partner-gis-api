0d5f1cfcb41ba2f997d0da721d41aca51d4db7ac
Ruan Caetano <ruansouza_caetano@hotmail.com>
2022-10-03 23:55:44 -0300

    build: add typeorm npm deps

---
+++ src/infra/database/mysql/typeorm.config.ts
@@ -0,0 +1,30 @@
+import { resolve } from "path";
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
+
+export default {
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "root",
+  password: "development",
+  database: "partner-gis",
+  logging: true,
+  namingStrategy: new SnakeNamingStrategy(),
+  entities: [
+    resolve(
+      __dirname,
+      "..",
+      "..",
+      "repositories",
+      "mysql",
+      "**",
+      "*.entity.ts"
+    ),
+  ],
+  migrations: [resolve(__dirname, "migrations", "*.ts")],
+  seeds: [resolve(__dirname, "seeds", "*.ts")],
+  cli: {
+    migrationsDir: resolve(__dirname, "migrations"),
+    seedsDir: [resolve(__dirname, "seeds")],
+  },
+};
